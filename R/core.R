4c83e0c339b76b71b66eb995a895d22e32ed93ef
Bart6114 <bartsmeets86@gmail.com>
2016-10-10 12:12:04 +0200

    travis + lint

--- R/core.R
+++ R/core.R
@@ -1,6 +1,6 @@
-SimmerOptim<-R6::R6Class(
+SimmerOptim <- R6::R6Class(
   "SimmerOptim",
-  public=list(
+  public = list(
     sim_expr = NULL,
     objective = NULL,
     .results = NULL,
@@ -14,7 +14,7 @@ SimmerOptim<-R6::R6Class(
     },
 
     results = function(objective, params, best_run, ...){
-      if(missing(objective)) return(self$.results)
+      if (missing(objective)) return(self$.results)
       else{
         self$.results <- c(list(objective = objective,
                                 params = params),
@@ -32,7 +32,7 @@ SimmerOptim<-R6::R6Class(
 #'
 #' @export
 run_instance <- function(sim_expr, ...){
-  temp_env <- new.env(parent=globalenv())
-  assign(".opt", opt_func(...), envir=temp_env)
-  eval(body(sim_expr), envir=temp_env)
+  temp_env <- new.env(parent = globalenv())
+  assign(".opt", opt_func(...), envir = temp_env)
+  eval(body(sim_expr), envir = temp_env)
 }
