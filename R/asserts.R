d85f155e333c54cde1aee1c8ffa406720c71a556
Iñaki Úcar <i.ucar86@gmail.com>
2016-11-25 19:05:29 +0100

    fix waiting time in asserts

--- R/asserts.R
+++ R/asserts.R
@@ -7,7 +7,7 @@
 assert_waiting_time_max<-function(envs, max_val){
   tmp <-
     simmer::get_mon_arrivals(envs) %>%
-    dplyr::mutate_("waiting_time" = "end_time - start_time")
+    dplyr::mutate_("waiting_time" = "end_time - (start_time + activity_time)")
 
   max(tmp$waiting_time) < max_val
 
@@ -23,7 +23,7 @@ assert_waiting_time_max<-function(envs, max_val){
 assert_avg_waiting_time_max<-function(envs, max_val){
   tmp <-
     simmer::get_mon_arrivals(envs) %>%
-    dplyr::mutate_("waiting_time" = "end_time - start_time") %>%
+    dplyr::mutate_("waiting_time" = "end_time - (start_time + activity_time)") %>%
     dplyr::summarise_("waiting_time_avg" = "mean(waiting_time)")
 
   tmp$waiting_time_avg < max_val
