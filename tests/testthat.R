e9937a7b61d0dad69f71d4be14528eb0a6d1ff89
Iñaki Úcar <i.ucar86@gmail.com>
2016-12-21 14:53:23 +0100

    move from create_trajectory to trajectory in tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,9 @@
 library(testthat)
 library(simmer.optim)
 
+if (-1 == utils::compareVersion("3.5.1", as.character(utils::packageVersion("simmer"))))
+  trajectory <- create_trajectory
+
 test_check("simmer.optim")
+
+detach("package:simmer", unload = TRUE)
