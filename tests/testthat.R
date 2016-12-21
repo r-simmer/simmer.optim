6b5feed8e748bedf1cf023529d3c7ea1ad09a519
Iñaki Úcar <i.ucar86@gmail.com>
2016-12-21 15:27:35 +0100

    fix version comparing

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,7 +1,7 @@
 library(testthat)
 library(simmer.optim)
 
-if (-1 == utils::compareVersion("3.5.1", as.character(utils::packageVersion("simmer"))))
+if (-1 < utils::compareVersion("3.5.1", as.character(utils::packageVersion("simmer"))))
   trajectory <- create_trajectory
 
 test_check("simmer.optim")
