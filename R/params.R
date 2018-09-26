f6424bb254b4bcddb97c6b3cb5f4598d60778b00
Tyler Bruns <tyler.bruns@uptake.com>
2018-09-26 09:43:01 -0500

    ran roxygenise to update Rd files and fixed minor typo in params.R

--- R/params.R
+++ R/params.R
@@ -1,6 +1,6 @@
 #' A parameter vector of type discrete
 #'
-#' @param vec the orginal vector
+#' @param vec the original vector
 #' @export
 par_discrete<-function(vec){
   class(vec) <- c("ParDiscrete", class(vec))
@@ -9,7 +9,7 @@ par_discrete<-function(vec){
 
 #' A parameter vector of type continuous
 #'
-#' @param vec the orginal vector
+#' @param vec the original vector
 #' @export
 par_continuous<-function(vec){
   vec <- as.numeric(vec)
